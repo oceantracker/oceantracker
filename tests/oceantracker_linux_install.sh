e21ea51e9b472f57ffee372f8cd1cee6bb887014
rossv <ross.vennell@cawthron.org.nz>
2022-07-23 11:56:19 +1200

    0.3.000 usernote tweak

--- tests/oceantracker_linux_install.sh
+++ tests/oceantracker_linux_install.sh
@@ -1,12 +1,21 @@
-#if replacing exiting package delele existing
-rm oceantracker -rf
+# install ocean tracker
+# run from inside dir where install required
+
+rm oceantracker -rf # replacing any existing package
 git clone https://github.com/oceantracker/oceantracker.git
-cd oceantracker
+cd ./oceantracker
 python3 -m venv venv
 source ./venv/bin/activate
-python setup.py develop pip install -r ./requirements.txt
+python setup.py develop
+pip install -r ./requirements.txt
 
+# test by running one demo
+echo "Test run demo 60"
+cd ./demos
+python run_demos.py -n --d 2
 cd ..
+cd ..
+
 
 
 
