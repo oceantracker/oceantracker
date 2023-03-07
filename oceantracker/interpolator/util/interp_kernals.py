6b4591b1c9bda1734e3e2df138f2d4ee4c17f35f
Ross Vennell <ross.vennell@cawthron.org.nz>
2023-03-08 11:57:31 +1300

    bug fix vertical walk

--- oceantracker/interpolator/util/interp_kernals.py
+++ oceantracker/interpolator/util/interp_kernals.py
@@ -1,7 +1,6 @@
 from numba import njit
 
 #todo faster with signature!
-#todo make it work faster on vectors, gufunc???, ie
 @njit
 def kernal_linear_interp1D(x1, y1, x2, y2, x):
     # linearly interpolate  to location x between (x1,y1), (x2,y2))
