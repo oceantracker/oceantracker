3cfa45164601bdfbb43c96e3b78d27e62a326582
Ross Vennell <ross.vennell@cawthron.org.nz>
2023-03-31 14:45:24 +1300

    0.4.00.000 2023-03-29  first to version 0.4 in dev branch

--- installing/build_conda_developer_env_steps.bat
+++ installing/build_conda_developer_env_steps.bat
@@ -1,16 +1,26 @@
 conda create -n dev-oceantracker python=3.10.8
 conda activate dev-oceantracker
+conda config --add channels https://conda.anaconda.org/conda-forge
+conda config --add channels https://conda.anaconda.org/domdfcoding
+conda install -c anaconda numpy
 conda install -c anaconda scipy
+conda install -c conda-forge netcdf4
 conda install -c conda-forge matplotlib
 conda install -c conda-forge netcdf4
+conda install -c conda-forge pyproj
 conda install -c numba numba
 conda install -c conda-forge psutil
 conda install -c anaconda pyyaml
 conda install -c conda-forge pyproj
 conda install -c anaconda sphinx
-conda config --add channels https://conda.anaconda.org/conda-forge
-conda config --add channels https://conda.anaconda.org/domdfcoding
 conda install sphinx-toolbox
 conda install -c conda-forge rst2pdf
+conda install -c anaconda line_profiler
 conda install -c conda-forge pyinstrument
+git clone https://github.com/oceantracker/oceantracker.git
+cd ./oceantracker
+pip install --no-deps -e .
+
+
+
 
