d2a5a062c415644869fcbf21a98647b4d05c2606
Ross Vennell <ross.vennell@cawthron.org.nz>
2023-04-12 13:51:11 +1200

    added bug hunting

--- colborators/bug_hunting/hunt_bug.py
+++ colborators/bug_hunting/hunt_bug.py
@@ -1,3 +1,12 @@
-import oceantracker as ot
+from oceantracker import main
+from oceantracker.util import json_util
+ncase=0
 
-fn = 'paramsBWDFY20120.json'
+if ncase ==0:
+    fn = r'F:\OceanTrackerOuput\bug_hunting\bwdFY2012\paramsBWDFY20120.json'
+    params = json_util.read_JSON(fn)
+    params['reader']['input_dir'] = r'G:\Hindcasts_large\OceanNumNZ-2022-06-20\final_version\2010'
+    params['shared_params']['root_output_dir'] = r'F:\OceanTrackerOuput\bug_hunting'
+    params['case_list'] =  params['case_list'][23:25]
+
+main.run(params)
