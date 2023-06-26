9702fedf5390c3399d51a7e7776d75b3f23fa9ae
Ross Vennell <ross.vennell@cawthron.org.nz>
2023-06-27 07:36:03 +1200

    tweaks to docs

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 from setuptools import setup, find_packages
 
 setup(name='oceantracker',
-      python_requires='>3.8,<=3.11',
+      python_requires='>=3.10,<=3.11',
       version='0.4',
-      description='Off line Lagrangian particle tracking in the Ocean',
+      description='Fast offline Lagrangian particle tracking in the Ocean',
       author='',
       author_email='',
       url='https://oceantracker.github.io/oceantracker/',
