b2d1db7406d2d2a51321513f7e49d4ada1984ab9
RomainChaput <rchaput@rsmas.miami.edu>
2024-10-14 15:49:16 +1300

    Modified settle_in_polygon.py to include age dependent settlement and a competency period

--- oceantracker/trajectory_modifiers/settle_in_polygon.py
+++ oceantracker/trajectory_modifiers/settle_in_polygon.py
@@ -10,9 +10,10 @@ class SettleInPolygon(_BaseTrajectoryModifier):
     def __init__(self):
         # set up info/attributes
         super().__init__()  # required in children to get parent defaults
-        self.add_default_params({'polygon': {'points': PCC(None, is_required=True,is3D=False)},
+        self.add_default_params({'polygon_list': [],
                                  'probability_of_settlement': PVC(0.,float),
                                  'settlement_duration': PVC(0., float,min=0.),  #  time block stranding after stranding has occured
+                                 'beginning_competency': PVC(0., float, min=0.), # Age of first settlement
                                  })
         self.polygons = []
 
@@ -25,14 +26,14 @@ class SettleInPolygon(_BaseTrajectoryModifier):
         super().initial_setup()
 
         # set up polygons to test if particles inside
-        if 'points' not in self.params['polygon']:
-            si.msg_logger.msg('initialize: Polygon settlement, each polygon must be a dictionary with at least a "points" key as a list of coordinates', fatal_error=True)
-
-        a = np.asarray(self.params['polygon']['points'])
-        if a.shape[1] != 2:
-            raise Exception('initialize: parameter polygon_ points must be a list of lists of coordinate pairs, eg [[p1_x1,p1_y1],[p1_x2,p1_y2],..]]')
-
-        self.polygon = InsidePolygon(verticies=a)# do set up to speed inside using pre-calculation
+        for p in self.params['polygon_list'] :
+            if 'points' not in p:
+                si.msg_logger.msg('initialize: Polygon settlement, each polygon must be a dictionary with at least a "points" key as a list of coordinates', fatal_error=True)
+            a = np.asarray(p['points'])
+            if a.shape[1] != 2:
+                raise Exception('initialize: parameter polygon_ points must be a list of lists of coordinate pairs, eg [[p1_x1,p1_y1],[p1_x2,p1_y2],..]]')
+            
+            self.polygons.append(InsidePolygon(verticies=a))# do set up to speed inside using pre-calculation
 
         # add particle prop to track which are inside polygon, which will be automatically written to output
         si.add_class('particle_properties', class_name='CoreParticleProperty', name='is_stationary_in_polygon', dtype='int8')
@@ -45,16 +46,22 @@ class SettleInPolygon(_BaseTrajectoryModifier):
          
         part_prop = si.class_roles.particle_properties
 
-        # find those inside and freeze, only if they haven't been recently stationary
-        those_inside = self.polygon.inside_indices(part_prop['x'].used_buffer(), out= self.get_partID_buffer('B1'), active=active)
-        if those_inside.shape[0] > 0:
-
-            not_stationary = part_prop['status'].find_subset_where(those_inside, 'eq', si.particle_status_flags.moving)
-            rand = np.random.rand(not_stationary.shape[0])
-            settling = not_stationary[rand < self.params['probability_of_settlement']]
-            part_prop['status'].set_values(si.particle_status_flags.stationary, settling)
-            part_prop['is_stationary_in_polygon'].set_values(1, settling)
-            part_prop['time_of_settlement'].set_values(time_sec, settling)
+        # find particles age
+        competent_larvae = part_prop['age'].data > self.params['beginning_competency']
+        si.msg_logger.msg('Plankton : settle_in_polygon - %s particles ready to settle' % (competent_larvae.sum()) )
+        
+        if competent_larvae.any():
+            # find those inside and freeze, only if they haven't been recently stationary
+            competent_positions = part_prop['x'].data[competent_larvae]
+            for p in self.polygons:
+                those_inside = p.inside_indices(competent_positions.used_buffer(), out= self.get_partID_buffer('B1'), active=active)
+                if those_inside.shape[0] > 0:
+                    not_stationary = part_prop['status'].find_subset_where(those_inside, 'eq', si.particle_status_flags.moving)
+                    rand = np.random.rand(not_stationary.shape[0])
+                    settling = not_stationary[rand < self.params['probability_of_settlement']]
+                    part_prop['status'].set_values(si.particle_status_flags.stationary, settling)
+                    part_prop['is_stationary_in_polygon'].set_values(1, settling)
+                    part_prop['time_of_settlement'].set_values(time_sec, settling)
 
         # now look at all those stationary
         stationary = part_prop['status'].compare_all_to_a_value( 'eq', si.particle_status_flags.stationary, out=self.get_partID_buffer('B1'))
