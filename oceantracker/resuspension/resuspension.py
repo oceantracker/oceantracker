8ee8394af40cc603b9058251987c503a28be51c6
oceantracker <ross.vennell@cawthron.org.nz>
2025-01-16 11:38:00 +1300

    simplified resuspension

--- oceantracker/resuspension/resuspension.py
+++ oceantracker/resuspension/resuspension.py
@@ -2,10 +2,13 @@
 from oceantracker.util.parameter_checking import ParamValueChecker as PVC
 import numpy as np
 from oceantracker.resuspension._base_resuspension import BaseResuspension
-from oceantracker.util.numba_util import njitOT
+from oceantracker.util.numba_util import njitOT, njitOTparallel
 
-from numba import  njit
+from numba import  njit, prange
 from oceantracker.shared_info import shared_info as si
+
+status_on_bottom = int(si.particle_status_flags.on_bottom)
+status_moving= int(si.particle_status_flags.moving)
 
 class Resuspension(BaseResuspension):
     # based on
@@ -33,39 +36,34 @@ class Resuspension(BaseResuspension):
         info['resuspension_factor']= 2.0*0.4*si.settings.z0*si.settings.time_step/(1. - 2./np.pi)
         pass
 
-    def select_particles_to_resupend(self, active):
-        # compare to single critical value
-        # todo add comparison to  particles critical value from distribution, add new particle property to hold  individual critical values
-        part_prop = si.class_roles.particle_properties
-        on_bottom = part_prop['status'].compare_all_to_a_value('eq', si.particle_status_flags.on_bottom, out = self.get_partID_buffer('B1'))
-
-        # compare to critical friction velocity
-        resupend = part_prop['friction_velocity'].find_subset_where(on_bottom, 'gteq',self.params['critical_friction_velocity'], out=self.get_partID_subset_buffer('B1'))
-        return resupend
 
     # all particles checked to see if they need status changing
-    def update(self,n_time_step, time_sec, active):
+    def update(self,n_time_step, time_sec, alive):
         # do resupension
         self.start_update_timer()
         info = self.info
 
-        info['min_resuspension_jump_not_used'] = np.sqrt(info['resuspension_factor']*self.params['critical_friction_velocity'])
-
         # resuspend those on bottom and friction velocity exceeds critical value
         part_prop = si.class_roles.particle_properties
-        resuspend = self.select_particles_to_resupend(active)
 
-        self.resuspension_jump(part_prop['friction_velocity'].data, info['resuspension_factor'], part_prop['x'].data, part_prop['water_depth'].data,si.settings.z0, resuspend)
+        self.resuspension_jump(part_prop['friction_velocity'].data, part_prop['status'].data,
+                                self.params['critical_friction_velocity'],
+                               info['resuspension_factor'],
+                               part_prop['x'].data, part_prop['water_depth'].data,si.settings.z0, alive)
 
-        # any z out of bounds will  be fixed by find_depth cell at start of next time step
-        part_prop['status'].set_values(si.particle_status_flags.moving, resuspend)
 
         self.stop_update_timer()
 
     @staticmethod
     @njitOT
-    def resuspension_jump(friction_velocity, resuspension_factor, x, water_depth, z0, sel):
+    def resuspension_jump(friction_velocity, status,
+                          critical_friction_velocity,
+                          resuspension_factor, x, water_depth, z0, alive):
         # add entrainment jump up to particle z, Book: Lynch(2015) book, Particles in the coastal ocean  eq 9.26 and 9.28
-        for n in sel:
-            x[n, 2] = -water_depth[n] + z0 + np.sqrt(resuspension_factor*friction_velocity[n])*np.abs(np.random.randn())
-
+        for n in alive:
+            #todo threade prange version give diffent results in tracks only  .2m on average,
+            # so slight different if decision when threaded? use threads  and accept change for faster threaded code?
+            if status[n] == status_on_bottom and friction_velocity[n] >= critical_friction_velocity:
+                # do suspension
+                x[n, 2] = -water_depth[n] + z0 + np.sqrt(resuspension_factor*friction_velocity[n])*np.abs(np.random.randn())
+                status[n] = status_moving
