d0bddc0cb4fa00a916a65f75e071d6f81ff023a6
laurin steidle <laurin.steidle@uni-hamburg.de>
2025-02-11 18:30:55 +0100

    updated version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(name='oceantracker',
       python_requires='>=3.10,<3.12',
-      version='0.4.9',
+      version='0.4.1',
       description='Fast offline Lagrangian particle tracking in the Ocean',
       long_description=open('README.md').read(),
       author='Ross Vennell & Laurin Steidle',
