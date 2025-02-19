52b0cca82569526c86bcc666922eb66e84a57b59
oceantracker <ross.vennell@cawthron.org.nz>
2025-02-20 12:08:54 +1300

    doc edits, trap divide by zero in read stats

--- dev/show_grid_and_variables.py
+++ dev/show_grid_and_variables.py
@@ -4,13 +4,14 @@ from oceantracker.util.ncdf_util import NetCDFhandler
 n_case= 0
 match n_case:
     case 0:
-        # haurakin gulf
+        # hauraki gulf
         file_name= r'Z:\Hindcasts\NorthIsland\2024_hauraki_gulf_auck_uni\2020\01\schout_1.nc'
         x_var= 'SCHISM_hgrid_node_x'
         y_var = 'SCHISM_hgrid_node_y'
         tri_var= 'SCHISM_hgrid_face_nodes'
         depth_var= 'depth'
         one_based = True
+        vmax= 30
 
 
 nc = NetCDFhandler(file_name)
@@ -22,8 +23,8 @@ y = nc.read_a_variable(y_var)
 tri = nc.read_a_variable(tri_var) - one_based
 depth = nc.read_a_variable(depth_var)
 
-plt.tripcolor(x, y, tri[:, :3], depth, vmax=50)
-plt.triplot(x, y, tri[:, :3],lw=.5, c=[.8,.8,.8])
+plt.tripcolor(x, y, tri[:, :3], depth, vmax=vmax)
+plt.triplot(x, y, tri[:, :3], lw=.5, c=[.8,.8,.8])
 plt.colorbar()
 plt.show()
 
