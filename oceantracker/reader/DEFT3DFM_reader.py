76e9359ac50578ac0f40958565fc763c0ef52172
oceantracker <ross.vennell@cawthron.org.nz>
2025-05-14 15:37:50 +1200

    improved preprcesing hindcast compression

--- oceantracker/reader/DEFT3DFM_reader.py
+++ oceantracker/reader/DEFT3DFM_reader.py
@@ -43,8 +43,8 @@ class DELF3DFMreader(_BaseUnstructuredReader):
                         'water_depth': PVC('mesh2d_node_z', str, doc_str='maps standard internal field name to file variable name'),
                         'water_temperature': PVC('temp', str, doc_str='maps standard internal field name to file variable name'),
                         'salinity': PVC(None, str, doc_str='maps standard internal field name to file variable name'),
-                        'wind_stress': PVC(None, str, doc_str='maps standard internal field name to file variable name'),
-                        'bottom_stress': PVC(None, str, doc_str='maps standard internal field name to file variable name'),
+                        'wind_stress': PLC(None, str, doc_str='maps standard internal field name to file variable name'),
+                        'bottom_stress': PLC(['mesh2d_tausx', 'mesh2d_tausy'], str, doc_str='maps standard internal field name to file variable name'),
                         'A_Z_profile': PVC(None, str, doc_str='maps standard internal field name to file variable name for turbulent eddy viscosity, used if present in files'),
                         'water_velocity_depth_averaged': PLC(['mesh2d_ucx','mesh2d_ucy'], str, fixed_len=2,
                                                 doc_str='maps standard internal field name to file variable names for depth averaged velocity components, used if 3D "water_velocity" variables not available')
