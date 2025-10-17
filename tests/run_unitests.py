21417337b19002581d9b57bc579d15d0987246f6
rossv <ross.vennell@cawthron.org.nz>
2025-10-17 13:34:41 +1300

    unit test util #2

---
+++ tests/run_unitests.py
@@ -0,0 +1,42 @@
+import  argparse
+from os import path,chdir, mkdir
+from oceantracker import definitions
+from glob import glob
+import importlib
+import  sys
+
+from unit_tests import  ut_def
+from oceantracker.util import json_util, yaml_util
+import dev_runs.test_definitions
+import oceantracker.main
+
+if __name__ == "__main__":
+
+    args = ut_def.get_args()
+
+
+    test_dir =path.join(definitions.ot_root_dir,'tests')
+    info=[]
+    files_dir = path.join(test_dir,'dev_runs')
+    for n in glob(path.join(files_dir,'unit_*.py')):
+        name = path.split(n)[-1].split('.')[0]
+        info.append([ int(name.split('_')[2]),name])
+
+    if args.test:
+        test_list =[x for x in info if x[0]==args.test]
+
+    else:
+        # do all tests
+        test_list =info
+
+    sys.path.append(files_dir)
+
+    param_dir = path.join(files_dir,'test_param_files')
+    if not path.isdir(param_dir) : mkdir(param_dir)
+
+
+    for n, name in test_list:
+        p = importlib.import_module(name)
+        params = p.main(args)
+        #if params is not None:
+        #    json_util.write_JSON(path.join(param_dir,f'params_{name}.json'),params)
--- tests/unit_tests/ut_def.py
+++ tests/unit_tests/ut_def.py
@@ -7,15 +7,16 @@ from oceantracker import definitions
 image_dir= 'output' # where to write plots
 
 # package demo hindcasts
-root_hindcast_dir= path.join(path.dirname(definitions.package_dir),'tutorials_how_to','demo_hindcast')
-reader_demo_schisim3D=   dict( input_dir= path.join(root_hindcast_dir,'schsim3D'),
-                file_mask='demo_hindcast_schisim3D*.nc')
-reader_demo_ROMS= dict(input_dir=path.join(root_hindcast_dir, 'ROMS'),
-                file_mask='ROMS3D_00*.nc')
-reader_demo_schisim2D=   dict( input_dir= path.join(root_hindcast_dir,'schsim2D'),
-                file_mask='Random_order*.nc')
+demo_hindcast_dir= path.join(path.dirname(definitions.package_dir), 'tutorials_how_to', 'demo_hindcast')
+reader_demo_schisim3D=   dict(input_dir= path.join(demo_hindcast_dir, 'schsim3D'),
+                              file_mask='demo_hindcast_schisim3D*.nc')
+reader_demo_ROMS= dict(input_dir=path.join(demo_hindcast_dir, 'ROMS'),
+                       file_mask='ROMS3D_00*.nc')
+reader_demo_schisim2D=   dict(input_dir= path.join(demo_hindcast_dir, 'schsim2D'),
+                              file_mask='Random_order*.nc')
 
-def setup():
+def get_args():
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--test', type=int)
     parser.add_argument('-devmode', action='store_true')
@@ -31,6 +32,8 @@ def setup():
 
     return args
 
+def setup():
+    args = get_args()
     #schism demo points, polygons
     schism_data=dict(deep_point=[1594000, 5484200, -2], # deep water
                      deep_polygon=[[1597682., 5486972], [1598604, 5487275], [1598886, 5486464],
@@ -41,3 +44,6 @@ def setup():
                     class_name='PointRelease',  # class to use
                     release_interval=1800,  # seconds between releasing particles
                     pulse_size=5)
+
+
+    return args
