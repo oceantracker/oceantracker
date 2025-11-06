e691699dabc35c68b7d1b6eca9f3dc21a9c456a4
Laurin Steidle <laurin.steidle@cawthron.org.nz>
2025-11-07 10:41:16 +1300

    adding particle properties tests

--- tests/unit_tests/test_particle_properties.py
+++ tests/unit_tests/test_particle_properties.py
@@ -1,14 +1,12 @@
 from oceantracker.main import OceanTracker
 import pytest
 
-def test_age_decay(
-    base_settings,
-    reader_schism3D,
-    basic_point_release,
-    schism_release_locations,
-    a_pollutant,
-):
 
+@pytest.fixture
+def default_particle_properties_configuration(
+    base_settings, reader_schism3D, basic_point_release, schism_release_locations
+):
+    """Returns a pre-configured OceanTracker instance with common setup."""
     ot = OceanTracker()
     ot.settings(**base_settings)
     ot.add_class("reader", **reader_schism3D)
@@ -16,43 +14,99 @@ def test_age_decay(
         "release_groups",
         **{**basic_point_release, "points": schism_release_locations["deep_point"]},
     )
-    ot.add_class("particle_properties", **a_pollutant)  # Required by heat map config
-    case_info_file = ot.run()
+    return ot
 
+
+def test_age_decay(default_particle_properties_configuration, a_pollutant):
+    ot = default_particle_properties_configuration
+    ot.add_class("particle_properties", **a_pollutant)
+    case_info_file = ot.run()
     assert case_info_file is not None
 
-@pytest.mark.skip(reason="Not implemented yet")
-def test_distance_travelled():
-    pass
 
-@pytest.mark.skip(reason="Not implemented yet")
-def test_total_water_depth():
-    pass
+def test_distance_travelled(default_particle_properties_configuration):
+    ot = default_particle_properties_configuration
+    ot.add_class(
+        "particle_properties",
+        class_name="DistanceTravelled",
+    )
+    case_info_file = ot.run()
+    assert case_info_file is not None
 
-@pytest.mark.skip(reason="Not implemented yet")
-def test_water_speed():
-    pass
 
-@pytest.fixture
-def water_speed_property():
-    """Water speed vector magnitude property"""
-    return dict(
+def test_fractional_water_depth(default_particle_properties_configuration):
+    ot = default_particle_properties_configuration
+    ot.add_class(
+        "particle_properties",
+        class_name="FractionalWaterDepth",
+    )
+    case_info_file = ot.run()
+    assert case_info_file is not None
+
+
+def test_inside_polygons(
+    default_particle_properties_configuration, schism_release_locations
+):
+    ot = default_particle_properties_configuration
+    ot.add_class(
+        "particle_properties",
+        name="my_inside_poly",
+        class_name="oceantracker.particle_properties.inside_polygons.InsidePolygonsNonOverlapping2D",
+        polygon_list=schism_release_locations["polygons"],
+    )
+    case_info_file = ot.run()
+    assert case_info_file is not None
+
+
+def test_rouse_number(default_particle_properties_configuration):
+    ot = default_particle_properties_configuration
+    ot.add_class(
+        "velocity_modifiers",
+        name="my_terminal_velocity",
+        class_name="TerminalVelocity",
+        value=-0.001,
+    )
+    ot.add_class(
+        "particle_properties",
+        class_name="RouseNumber",
+        name="rouse_number",
+        terminal_velocity_name="my_terminal_velocity",
+    )
+    case_info_file = ot.run()
+    assert case_info_file is not None
+
+
+# @pytest.mark.skip()
+def test_particle_load(default_particle_properties_configuration):
+    ot = default_particle_properties_configuration
+    ot.add_class(
+        "particle_properties",
+        name="my_particle_load",
+        class_name="ParticleLoad",
+        initial_value=100,
+    )
+    case_info_file = ot.run()
+    assert case_info_file is not None
+
+
+def test_total_water_depth(default_particle_properties_configuration):
+    ot = default_particle_properties_configuration
+    ot.add_class(
+        "particle_properties",
+        name="total_water_depth",
+        class_name="TotalWaterDepth",
+    )
+    case_info_file = ot.run()
+    assert case_info_file is not None
+
+
+def test_water_speed(default_particle_properties_configuration):
+    ot = default_particle_properties_configuration
+    ot.add_class(
+        "particle_properties",
         name="water_speed",
         class_name="VectorMagnitude2D",
         vector_part_prop="water_velocity",
     )
-
-@pytest.fixture
-def distance_travelled_property():
-    """Distance travelled property"""
-    return dict(
-        class_name="DistanceTravelled",
-    )
-
-@pytest.fixture
-def age_decay_property():
-    """Age decay property"""
-    return dict(
-        class_name="AgeDecay",
-        name="test_decay",
-    )
+    case_info_file = ot.run()
+    assert case_info_file is not None
